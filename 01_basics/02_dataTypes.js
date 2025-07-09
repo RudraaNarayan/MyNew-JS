3456b4985bef7f15963edc184125443ed748eabc
RudraaNarayan <rudranaryanprusty@gmail.com>
2025-07-09 12:35:34 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3+
+    
+    3); // code readability should be high 
+
+console.log("Rudra");
+
+
+let name="Rudra"
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+// number => 2 is power 53
+// bigInt
+// string =>""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Rudra");
+console.log(typeof age); 
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

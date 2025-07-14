b48ff602b5d0f555a8904b360a605a2fcb2b8fe6
RudraaNarayan <rudranaryanprusty@gmail.com>
2025-07-14 05:36:43 +0000

    container config done

---
+++ 01_basics/strings.js
@@ -0,0 +1,17 @@
+const name ="Rudra"
+const repoCount = 20
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Rudraaa')
+
+// console.log(gameName[0]);  //R
+// console.log(gameName.__proto__);  //{}
+
+
+// console.log(gameName.length);  //7
+// console.log(gameName.toUpperCase());  //RUDRAAA
+console.log(gameName.charAt(2));  //d
+onsole.log(gameName.charAt(2));
